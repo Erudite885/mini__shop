b481c39864c0d979f5c6f8156e17ea9e2700ac61
Erudite885 <erudite885@gmail.com>
2023-01-17 09:01:39 +0100

    starting with oop code

---
+++ assets/scripts/app.js
@@ -0,0 +1,29 @@
+const productList = {
+  products: [
+    { title: "Carpet", imgUrl: "", price: 10, description: "a carpet" },
+    { title: "Pillow", imgUrl: "", price: 10, description: "a pillow" },
+  ],
+  render() {
+    const renderHook = document.getElementById("app");
+    const prodList = document.createElement("ul");
+    prodList.className = "product-list";
+    for (const product of this.products) {
+      const prodElement = document.createElement("li");
+      prodElement.className = "product-item";
+      prodElement.innerHTML = `
+            <div>
+                <img src="${product.imgUrl}" alt="${product.title}" >
+                <div class="product-item__content">
+                <h2>${product.title}</h2>
+                <h3>${product.price}</h3>
+                <p>${product.description}</p>
+                <button>Add to Cart</button>
+                </div>
+            </div>
+        `;
+      prodList.append(prodElement);
+    }
+    renderHook.append(prodList);
+  },
+};
+productList.render();
