516b5802e56d5f76e33aa7802e9fa5b96d221040
Erudite885 <erudite885@gmail.com>
2023-01-17 18:20:59 +0100

    inheritance everywhere

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,11 +4,11 @@ class Product {
   //   description;
   //   price;
 
-  constructor(title, img, desc, px) {
+  constructor(title, img, desc, price) {
     this.title = title;
     this.imageUrl = img;
     this.description = desc;
-    this.price = px;
+    this.price = price;
   }
 }
 
@@ -20,7 +20,7 @@ class ElementAttribute {
 }
 class Component {
   constructor(renderHookId) {
-    this.hook = renderHookId;
+    this.hookId = renderHookId;
   }
 
   createRootElement(tag, cssClasses, attributes) {
@@ -38,62 +38,12 @@ class Component {
   }
 }
 
-class ProductList {
-  products = [
-    new Product("Carpet", "https://www.carpet.com", "a carpet", 10),
-    new Product("Pillow", "https://www.pillow.com", "a Pillow", 30),
-  ];
-  constructor() {}
-  render() {
-    const prodList = document.createElement("ul");
-    prodList.className = "product-list";
-    for (const product of this.products) {
-      const productItem = new ProductItem(product);
-      const prodElement = productItem.render();
-      prodList.append(prodElement);
-    }
-    return prodList;
-  }
-}
-
-class ProductItem {
-  constructor(product) {
-    this.product = product;
-  }
-
-  addToCart() {
-    App.addProductToCart(this.product);
-  }
-
-  render() {
-    const prodElement = document.createElement("li");
-    prodElement.className = "product-item";
-    prodElement.innerHTML = `
-            <div>
-                <img src="${this.product.imgUrl}" alt="${this.product.title}" >
-                <div class="product-item__content">
-                <h2>${this.product.title}</h2>
-                <h3>${this.product.price}</h3>
-                <p>${this.product.description}</p>
-                <button>Add to Cart</button>
-                </div>
-            </div>
-        `;
-    const addToCartButton = prodElement.querySelector("button");
-    addToCartButton.addEventListener("click", this.addToCart.bind(this));
-    return prodElement;
-  }
-}
-
 class ShoppingCart extends Component {
   items = [];
 
-  set cartItems(val) {
-    this.items = val;
-    this.totalOutput.innerHTML = `
-    <h2>
-        Total Amount: \$${this.totalAmount.toFixed(2)}
-    </h2>`;
+  set cartItems(value) {
+    this.items = value;
+    this.totalOutput.innerHTML = `<h2>Total Amount: \$${this.totalAmount}</h2>`;
   }
 
   get totalAmount() {
@@ -114,28 +64,80 @@ class ShoppingCart extends Component {
   }
 
   render() {
-    const cartElement = this.createRootElement("section", "cart");
-    cartElement.innerHTML = `
-        <h2>Total Amount: \$${0} </h2>
-        <button>Order Now</button>
+    const cartEl = this.createRootElement("section", "cart");
+    cartEl.innerHTML = `
+    <h2>Total Amount: \$${0} </h2>
+    <button>Order Now</button>
     `;
-    // cartElement.className = "cart";
-    this.totalOutput = cartElement.querySelector("h2");
-    return cartElement;
+    this.totalOutput = cartEl.querySelector("h2");
+    // return cartElement;
+  }
+}
+
+class ProductItem extends Component {
+  constructor(product, renderHookId) {
+    super(renderHookId);
+    this.product = product;
+  }
+
+  addToCart() {
+    App.addProductToCart(this.product);
+  }
+
+  render() {
+    const prodElement = this.createRootElement("li", "product-item");
+    // prodElement.className = "product-item";
+    prodElement.innerHTML = `
+            <div>
+                <img src="${this.product.imgUrl}" alt="${this.product.title}" >
+                <div class="product-item__content">
+                <h2>${this.product.title}</h2>
+                <h3>${this.product.price}</h3>
+                <p>${this.product.description}</p>
+                <button>Add to Cart</button>
+                </div>
+            </div>
+        `;
+    const addToCartButton = prodElement.querySelector("button");
+    addToCartButton.addEventListener("click", this.addToCart.bind(this));
+    // return prodElement;
+  }
+}
+class ProductList extends Component {
+  products = [
+    new Product("Carpet", "https://www.nothing.com", "a carpet", 10),
+    new Product("Pillow", "https://www.nothing.com", "a Pillow", 30),
+  ];
+
+  constructor(renderHookId) {
+    super(renderHookId);
+  }
+
+  render() {
+    this.createRootElement("ul", "product-list", [
+      new ElementAttribute("id", "prod-list"),
+    ]);
+    // prodList.id = "prod-list";
+    // prodList.className = "product-list";
+    for (const prod of this.products) {
+      const productItem = new ProductItem(prod, "prod-list");
+      productItem.render();
+      //   prodList.append(prodElement);
+    }
+    // return prodList;
   }
 }
 
 class Shop {
   render() {
-    const renderHook = document.getElementById("app");
+    // const renderHook = document.getElementById("app");
 
     this.cart = new ShoppingCart("app");
-    // const cartEl = this.cart.render();
-    const productList = new ProductList();
-    const prodListEl = productList.render();
+    this.cart.render(); // i wasnt calling the cart.render earlier so i had the undefined error
+    const productList = new ProductList("app");
+    productList.render();
 
-    // renderHook.append(cartEl);
-    renderHook.append(prodListEl);
+    // renderHook.append(prodListEl);
   }
 }
 
